360a3a4ae0d75ae1048dc5d0721c39fded2fe463
vmdoi <rodriguesorlen@gmail.com>
2022-07-21 19:24:02 +0000

    booo

--- src/lib/music.ts
+++ src/lib/music.ts
@@ -1,9 +1,17 @@
-import {Synth, type SynthOptions} from 'tone';
+import * as Tone from 'tone';
 
-//create a synth and connect it to the main output (your speakers)
-export const synth = new Synth().toDestination();
-
+export const boring = new Tone.Synth().toDestination();
+export const noiseSynth = new Tone.NoiseSynth().toDestination();
+export const sawSynth = new Tone.MonoSynth({
+	oscillator: {
+		type: "sawtooth"
+	},
+	envelope: {
+		attack: 0.1
+	}
+}).toDestination();
 //play a middle 'C' for the duration of an 8th note
-export function play(player: Synth<SynthOptions>){
-    player.triggerAttackRelease("C4", "8n");
-}
+export function play(player: Tone.Synth<Tone.SynthOptions>|Tone.MonoSynth|Tone.NoiseSynth, frequency: Tone.Unit.Frequency){
+    player.triggerAttackRelease(frequency, "8n");
+};
+
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
     import { onMount } from 'svelte';
+    import type * as Tone from 'tone';
     var handleClick: svelte.JSX.MouseEventHandler<HTMLButtonElement>|null|undefined;
-
+    let frequency :Tone.Unit.Frequency ="A4";
+    var notes = ["C2", "E4", "G4", "Bb4"]
+    
+    var played = 0
+    var synth: Tone.Synth<Tone.SynthOptions>|Tone.MonoSynth|Tone.NoiseSynth;
     onMount(async()=>{
-    const {play, synth} = (await import ('$lib/music'))
+    let {play, boring, sawSynth, noiseSynth} = (await import ('$lib/music'))
+    synth = noiseSynth
      handleClick = ()=> {
-		play(synth)
-        
+        synth = sawSynth
+        frequency = notes[ played % notes.length ]
+        played ++
+		play(synth ,frequency)
 	}
     });
 </script>
 <p>the button makes a noise</p>
-<button on:click={handleClick}> click me</button>
+<button on:click={handleClick}> click me</button> 
+the frequency is {frequency}
+
